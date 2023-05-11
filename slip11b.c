d6b5ff6158232642aa83187a3b4a83769f1eb849
Shad0w35 <92471694+Shad0w35@users.noreply.github.com>
2023-05-11 08:53:46 +0530

    Update slip11b.c

--- slip11b.c
+++ slip11b.c
@@ -1,41 +1,46 @@
-#include<mpi.h>
-#include<stdio.h>
-#include<stdlib.h>
-void allocate(int a[],int n)
-{
-int i;
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-for(i=0;i<n;i++)
-a[i]=rand()%50;
-}
+#define ARRAY_SIZE 1000
 
-int main(int argc,char *argv[])
-{
-int rank,size,n,max=0,min=51,count,*a=NULL,i;
-rand();
-MPI_Init(&argc,&argv);
-MPI_Comm_size(MPI_COMM_WORLD,&size);
-MPI_Comm_rank(MPI_COMM_WORLD,&rank);
-n=atoi(argv[1]);
-printf("\n Rank=%d n=%d\n",rank,n);
-count=n/size;
-if(rank==0)
-{
-a=(int *) malloc(n*sizeof(int));
+int main(int argc, char** argv) {
+    int rank, size;
+    int i, min_local, min_global;
+    int* array = NULL;
 
-allocate(a,n);
-}
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-for(i=0;i<count;i++)
-{
-printf("\t%d\n",a[i]);
-if(a[i]>max)
-max=a[i];
-if(a[i]<min)
-min=a[i];
-}
+    // Allocate memory for the array on each process
+    array = (int*)malloc(ARRAY_SIZE * sizeof(int));
 
-printf("\nmax=%d\t min=%d",max,min);
+    // Generate random numbers on each process
+    srand(rank + 1);
+    for (i = 0; i < ARRAY_SIZE; i++) {
+        array[i] = rand();
+    }
 
-MPI_Finalize();
+    // Find the local minimum
+    min_local = array[0];
+    for (i = 1; i < ARRAY_SIZE; i++) {
+        if (array[i] < min_local) {
+            min_local = array[i];
+        }
+    }
+
+    // Use MPI_Reduce to find the global minimum
+    MPI_Reduce(&min_local, &min_global, 1, MPI_INT, MPI_MIN, 0, MPI_COMM_WORLD);
+
+    // Print the result from the root process
+    if (rank == 0) {
+        printf("Minimum number: %d\n", min_global);
+    }
+
+    // Clean up
+    free(array);
+    MPI_Finalize();
+
+    return 0;
 }
