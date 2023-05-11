fc3a701ab32a2152e7d18ec9011a11fb108ed443
Shad0w35 <92471694+Shad0w35@users.noreply.github.com>
2023-05-11 09:15:56 +0530

    Update slip1a.c

--- slip1a.c
+++ slip1a.c
@@ -1,65 +1,120 @@
-#include<stdio.h>
- main()
- {
-   int i,j,p,r,alloc[10][10],need[10][10],max[10][10],available[10];
-   printf("\n Enter the how many process you want:");
-   scanf("%d",&p);
-   printf(" Enter the how many resources you want:");
-   scanf("%d",&r);
-   printf("\n Enter the allocation matrix:\n");
-   for(i=0;i<p;i++)
-    {
-     
-     printf(" P%d\t",i);
-      for(j=0;j<r;j++)
-       scanf("%d",&alloc[i][j]);
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define MAX_PROCESSES 10
+#define MAX_RESOURCES 10
+
+int available[MAX_RESOURCES];
+int max[MAX_PROCESSES][MAX_RESOURCES];
+int allocation[MAX_PROCESSES][MAX_RESOURCES];
+int need[MAX_PROCESSES][MAX_RESOURCES];
+int num_processes, num_resources;
+
+void acceptAvailable() {
+    int i;
+    printf("Enter the available resources:\n");
+    for (i = 0; i < num_resources; i++) {
+        scanf("%d", &available[i]);
     }
-   printf("\n Enter the maximum demand of matrix:\n");
-   for(i=0;i<p;i++)
-    {
-     printf(" P%d\t",i);
-      for(j=0;j<r;j++)
-       scanf("%d",&max[i][j]);
-    }
-   printf("\n Enter the available resource matrix: ");
-   for(i=0;i<r;i++)
-     {
-       scanf("%d",&available[i]);
-       
-     }
-   printf("\n The contents of need array is:");
-   for(i=0;i<p;i++)
-    {
-     printf("\n P%d",i);
-      for(j=0;j<r;j++)
-       {
-	 need[i][j]=max[i][j]-alloc[i][j];
-	 printf("\t%d",need[i][j]);
-       }
-    }
-printf("\n The contents Allocation Matrix:");
-   for(i=0;i<p;i++)
-    {
-     printf("\n P%d",i);
-      for(j=0;j<r;j++)
-       {
-	 printf("\t%d",alloc[i][j]);
-       }
-    }
-printf("\n The contents Max Matrix is:");
-   for(i=0;i<p;i++)
-    {
-     printf("\n P%d",i);
-      for(j=0;j<r;j++)
-       {
-	 printf("\t%d",max[i][j]);
-       }
-    }
-printf("\n The contents Available Matrix is:");
-   for(i=0;i<r;i++)
-     {
-             
-	 printf("\t%d",available[i]);
-       }
-    
 }
+
+void displayAllocationMax() {
+    int i, j;
+    printf("Allocation Matrix:\n");
+    for (i = 0; i < num_processes; i++) {
+        for (j = 0; j < num_resources; j++) {
+            printf("%d ", allocation[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Max Matrix:\n");
+    for (i = 0; i < num_processes; i++) {
+        for (j = 0; j < num_resources; j++) {
+            printf("%d ", max[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void displayNeed() {
+    int i, j;
+    printf("Need Matrix:\n");
+    for (i = 0; i < num_processes; i++) {
+        for (j = 0; j < num_resources; j++) {
+            printf("%d ", need[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void displayAvailable() {
+    int i;
+    printf("Available Resources:\n");
+    for (i = 0; i < num_resources; i++) {
+        printf("%d ", available[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int choice;
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &num_processes);
+
+    printf("Enter the number of resources: ");
+    scanf("%d", &num_resources);
+
+    int i, j;
+    printf("Enter the allocation matrix:\n");
+    for (i = 0; i < num_processes; i++) {
+        for (j = 0; j < num_resources; j++) {
+            scanf("%d", &allocation[i][j]);
+        }
+    }
+
+    printf("Enter the max matrix:\n");
+    for (i = 0; i < num_processes; i++) {
+        for (j = 0; j < num_resources; j++) {
+            scanf("%d", &max[i][j]);
+            need[i][j] = max[i][j] - allocation[i][j];
+        }
+    }
+
+    do {
+        printf("\nBanker's Algorithm\n");
+        printf("1. Accept Available Resources\n");
+        printf("2. Display Allocation and Max\n");
+        printf("3. Display Need Matrix\n");
+        printf("4. Display Available Resources\n");
+        printf("5. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                acceptAvailable();
+                break;
+            case 2:
+                displayAllocationMax();
+                break;
+            case 3:
+                displayNeed();
+                break;
+            case 4:
+                displayAvailable();
+                break;
+            case 5:
+                printf("Exiting the program.\n");
+                break;
+            default:
+                printf("Invalid choice. Please try again.\n");
+                break;
+        }
+    } while (choice != 5);
+
+    return 0;
+}
+``
